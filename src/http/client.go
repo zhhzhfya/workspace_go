50ec5339980e0833373f08d500bc99d3f4a64efd
zhenya.zhang <zhhzhfya@163.com>
2018-09-18 17:03:24 +0800

    add flies
    add files

--- src/http/client.go
+++ src/http/client.go
@@ -4,12 +4,15 @@ import (
 	"net/http"
 	"fmt"
 	"net/http/httputil"
+	"math/rand"
 )
 
 func main() {
 	request, err := http.NewRequest(http.MethodGet, "http://www.imooc.com", nil)
 	request.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36")
-	resp, err := http.Get("http://www.imooc.com")
+	resp, err := http.DefaultClient.Do(request)
+
+	//resp, err := http.Get("http://www.imooc.com")
 	if err != nil {
 		panic(err)
 	}
@@ -19,4 +22,6 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("%s\n", s)
+	i := rand.Intn(122)
+	fmt.Printf("%d",i)
 }
